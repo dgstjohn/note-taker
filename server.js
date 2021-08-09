b1a5159656415641b256d2c53cb18edd4bb87352
dgstjohn <dgstjohn@gmail.com>
2021-08-08 21:42:23 -0400

    finally figured out how to access the CSS/JS

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 // npm package to create unique IDs for new note objects
 const uniqid = require('uniqid');
 const notes = require('./db/db.json');
+// process.env.PORT for the non-local Heroku version
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Sets up path to the CSS and JS
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Sets up the HTML routes for the homepage and notes page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
@@ -18,36 +22,37 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-// set up API routes -- res.json
-// GET /api/notes should read the db.json file and return all saved notes as JSON
+// Sets up GET for the API routes
+// GET /api/notes reads the db.json file and returns all saved notes as JSON
 
 app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'UTF-8', (err, text) => {
-    let currentNotes = JSON.parse(text);
+        let currentNotes = JSON.parse(text);
         res.send(currentNotes);
     })
 });
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
-// each note will need a unique ID
+// Sets up POST for the API routes
+// /api/notes receives a new note to save on the request body, adds it to the db.json file, and then returns the new note to the client
+// Using the Uniqid npm package to generate an id for the notes
 
 app.post('/api/notes', (req, res) => {
     // get the existing notes
     fs.readFile('db/db.json', 'UTF-8', (err, text) => {
         let currentNotes = JSON.parse(text);
         // create the new note
-            let newNote = {
-                title: req.body.title,
-                text: req.body.text,
-                id: uniqid()
-            }
-            // add the new note to db.json
-            currentNotes.push(newNote);
-            // overwrite old db.json with new version
-            fs.writeFile('db/db.json', JSON.stringify(currentNotes), (err) => {
-                if (err) throw err;
-            })
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqid()
+        }
+        // add the new note to db.json
+        currentNotes.push(newNote);
+        // overwrite old db.json with new version
+        fs.writeFile('db/db.json', JSON.stringify(currentNotes), (err) => {
+            if (err) throw err;
         })
+    })
 
 })
 
